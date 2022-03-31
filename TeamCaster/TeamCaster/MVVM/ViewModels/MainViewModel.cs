7e25cf74659ccb54f7e62790861e9e939f534fb5
CyCJIuK47 <87585798+CyCJIuK47@users.noreply.github.com>
2022-03-31 15:38:45 +0300

    MainWindow Template

---
+++ TeamCaster/TeamCaster/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamCaster.MVVM.ViewModels.Base;
+
+namespace TeamCaster.MVVM.ViewModels
+{
+    class MainViewModel : ViewModel
+    {
+
+    }
+}
